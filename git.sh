f8eed20852b8de14a1a1be1e12738d2ee2db7b51
murftech <murftech7@gmail.com>
2026-09-02 04:58:50 +0800

    ready for 1st release

--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 git init
 git add .
 git commit -m "initial commit: sparkutils package (getspark, functions)"   
-gh repo create helper_sparkutils --public --source. 
+gh repo create helper_sparkutils --public --source=. --remote=origin --push 
 
+git remote -v
+gh repo view --web
